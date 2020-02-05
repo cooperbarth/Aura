91545f5600e3fe5c38fc06c15b3fb6f0611af8ba
Cooper Barth <cooperfbarth@gmail.com>
2020-02-04 21:10:14 -0600

    collision and mouse movement

--- Aura/Assets/Scripts/PlayerMovement.cs
+++ Aura/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5.0f;
-    public float rotationSpeed = 90.0f;
+    // public float rotationSpeed = 90.0f;
+    public bool enabled = true;
     public bool invertYAxis;
 
     private Vector3[] transformRotations = new Vector3[5];
@@ -14,10 +15,11 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 resetRotation = new Vector3(0, 0, 0);
     private bool compare;
 
-
     void Update()
     {
-        ProcessInput(invertYAxis, invert);
+        if (enabled) {
+            ProcessInput(invertYAxis, invert);
+        }
     }
 
     void ProcessInput(bool _invertYAxis, int _invert)
@@ -26,72 +28,44 @@ public class PlayerMovement : MonoBehaviour
         _invert = _invertYAxis ? -1 : 1;
 
         //Strafe Right
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D))
         {
             transform.position += transform.right * speed * Time.deltaTime;
         }
         //Strafe Left
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.position += -transform.right * speed * Time.deltaTime;
         }
         //Move Forward
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.W))
         {
             transform.position += transform.forward * speed * Time.deltaTime;
         }
         //Move Back
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.S))
         {
             transform.position += -transform.forward * speed * Time.deltaTime;
         }
+
+        /*
         //Rotate Right
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime, 0));
         }
         //Rotate Left
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Rotate(new Vector3(0, -rotationSpeed * Time.deltaTime, 0));
-        }
-        //Rotate Up (Down if invertYAxis = True)
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Rotate(new Vector3(_invert * -rotationSpeed * Time.deltaTime, 0, 0));
-        }
-        //Rotate Down (Up if invertYAxis = True)
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Rotate(new Vector3(_invert * rotationSpeed * Time.deltaTime, 0, 0));
-        }
-        //Roll Right
-        if (Input.GetKey(KeyCode.E))
-        {
-            transform.Rotate(new Vector3(0, 0, -rotationSpeed * Time.deltaTime));
-        }
-        //Roll Left
-        if (Input.GetKey(KeyCode.Q))
-        {
-            transform.Rotate(new Vector3(0, 0, rotationSpeed * Time.deltaTime));
-        }
-
-        //Reset Camera Rotation
-        if (Input.GetKey(KeyCode.R))
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
         }
         //Reset Camera Position
         if (Input.GetKey(KeyCode.T))
         {
             transform.position = new Vector3(0, 0, 0);
         }
+        */
 
-        if (Input.GetKey(KeyCode.BackQuote))
-        {
-            transform.position = new Vector3(0, 0, 0);
-            transform.eulerAngles = new Vector3(0, 0, 0);
-        }
         //Cache and Recall Camera Positions
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
         {
@@ -132,5 +106,13 @@ public class PlayerMovement : MonoBehaviour
         {
             Debug.Log("No preset cached");
         }
+    }
+
+    void EnableMovement() {
+        enabled = true;
+    }
+
+    void DisableMovement() {
+        enabled = false;
     }
 }
