14945d7a0cf05469112ed3bac44601c249c18143
Cooper Barth <cooperfbarth@gmail.com>
2020-03-05 02:15:46 -0600

    added elevator shaft

---
+++ Aura/Assets/Scripts/EnableGameObjectsOnTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EnableGameObjectsOnTrigger : MonoBehaviour
+{
+    public bool once = true;
+    public GameObject[] objects;
+
+    private bool triggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!(once && triggered))
+        {
+            triggered = true;
+            foreach (GameObject obj in objects)
+            {
+                obj.SetActive(true);
+            };
+        }
+    }
+}
