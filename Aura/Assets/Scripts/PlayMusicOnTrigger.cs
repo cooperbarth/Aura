8fd77d1ae3e26c68c900394c7912a98c90dace5c
Cooper Barth <cooperfbarth@gmail.com>
2020-03-04 18:29:19 -0600

    added music

---
+++ Aura/Assets/Scripts/PlayMusicOnTrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayMusicOnTrigger : MonoBehaviour
+{
+    public AudioSource intro;
+    public AudioSource main;
+    public bool once = true;
+
+    private bool introPlayed = false;
+    private bool mainStarted = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!(once && introPlayed))
+        {
+            print("hewwo");
+            intro.Play(0);
+            introPlayed = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (introPlayed && !mainStarted && !intro.isPlaying)
+        {
+            mainStarted = true;
+            main.Play(0);
+        }
+    }
+}
