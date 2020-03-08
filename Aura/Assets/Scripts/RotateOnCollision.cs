53e306ba7f0bf4bfde8e4914d3c7e1b374f0d163
Cooper Barth <cooperfbarth@gmail.com>
2020-03-08 00:28:50 -0600

    finished puzzle 2

--- Aura/Assets/Scripts/RotateOnCollision.cs
+++ Aura/Assets/Scripts/RotateOnCollision.cs
@@ -16,9 +16,7 @@ public class RotateOnCollision : MonoBehaviour
 
     private async void OnTriggerEnter(Collider collider)
     {
-        print("hi");
         if (rotated) { return; }
-        print("hi again");
         obj.transform.localRotation = Quaternion.Euler(new Vector3(90, 0, 0));
         rotated = true;
         await Task.Delay(Convert.ToInt32(waitTimeSeconds * 1000));
---
+++ Aura/Assets/Scripts/TrafficLight.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class TrafficLight : MonoBehaviour
+{
+    public Material[] materials;
+    public GameObject[] platforms;
+
+    private new Renderer renderer;
+
+    public void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        SwitchColor();
+    }
+
+    private async void SwitchColor()
+    {
+        while (true)
+        {
+            renderer.material = materials[0];
+            await Task.Delay(Convert.ToInt32(10000));
+            renderer.material = materials[1];
+            await Task.Delay(Convert.ToInt32(3000));
+            renderer.material = materials[2];
+            RotatePlatforms(true);
+            await Task.Delay(Convert.ToInt32(500));
+            RotatePlatforms(false);
+        }
+    }
+
+    private void RotatePlatforms(bool on)
+    {
+        if (on)
+        {
+            foreach(GameObject obj in platforms)
+            {
+                obj.transform.localRotation = Quaternion.Euler(new Vector3(90, 0, 0));
+            }
+        }
+        else
+        {
+            foreach(GameObject obj in platforms)
+            {
+                obj.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            }
+        }
+    }
+}
