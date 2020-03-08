c68cf1d84d02544125a3bd62d4d4190b2cc151ef
Cooper Barth <cooperfbarth@gmail.com>
2020-03-08 01:23:20 -0600

    minor edits

---
+++ Aura/Assets/Scripts/SetAudioSourceLoop.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SetAudioSourceLoop : MonoBehaviour
+{
+    public new AudioSource audio;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        audio.loop = true;
+    }
+}
